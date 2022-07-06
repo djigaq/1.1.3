4af430cdd553da602f6b9967276045a4daf28eef
djigaq <103529027+djigaq@users.noreply.github.com>
2022-07-06 16:42:12 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -19,16 +19,28 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Statement statement = CONNECTION.createStatement()) {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS Users " +
                     "(ID BIGINT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(255), LastName VARCHAR(255), Age INT)");
+            CONNECTION.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                CONNECTION.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     public void dropUsersTable() {
         try (Statement statement = CONNECTION.createStatement()) {
             statement.executeUpdate("DROP TABLE IF EXISTS Users");
+            CONNECTION.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                CONNECTION.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
@@ -38,8 +50,14 @@ public class UserDaoJDBCImpl implements UserDao {
             ps.setString(2, lastName);
             ps.setByte(3, age);
             ps.executeUpdate();
+            CONNECTION.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                CONNECTION.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
@@ -47,8 +65,14 @@ public class UserDaoJDBCImpl implements UserDao {
         try (PreparedStatement ps = CONNECTION.prepareStatement("DELETE FROM Users WHERE ID = ?")) {
             ps.setLong(1, id);
             ps.executeUpdate();
+            CONNECTION.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                CONNECTION.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
@@ -61,9 +85,17 @@ public class UserDaoJDBCImpl implements UserDao {
                         rs.getString( "LastName"), rs.getByte("Age"));
                 user.setId(rs.getLong("ID"));
                 users.add(user);
+                rs.close();
+                CONNECTION.commit();
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                CONNECTION.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
         }
 
         return users;
@@ -72,8 +104,14 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Statement statement = CONNECTION.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE Users");
+            CONNECTION.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                CONNECTION.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
